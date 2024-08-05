9c0aff3d254531e9ffaae4f2167fa8770b17b156
ArjunKanojia <arjunkanojia040@gmail.com>
2024-08-06 00:19:28 +0530

    coding Shuttle 1.2 setup

---
+++ src/test/java/com/codingShuttle/Basics/BasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.Basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
